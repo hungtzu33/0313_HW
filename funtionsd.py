0099a6bab5d156418898c576fd249a1b786915be
Vivian <vivian880918@gmail.com>
2023-04-19 10:43:04 +0800

    0313_HW

--- funtionsd.py
+++ funtionsd.py
@@ -4,6 +4,8 @@ def my_sd(input):
     print (f'Input : {input}')
     length = len(input)
     sum = 0
+    return new_func(input, length, sum)
+def new_func(input, length, sum):
     mean = statistics.mean(input)
     print(f'Mean :{mean}')
     for x in input:
@@ -18,7 +20,7 @@ input = [10,20,30]
 
 # 2.Process
 answer = my_sd(input)
-
+answer=round(answer,3)
 # 3.Output
 print(f'anwser:{answer}')
 
